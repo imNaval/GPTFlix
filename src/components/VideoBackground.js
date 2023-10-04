f064652c8d4545316c54f47e2216d4e082644a41
imNaval <nehranvl2017@gmail.com>
2023-10-04 22:06:43 +0530

    abstract logic of fetch trailer

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,31 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import { TMDB_API_OPTIONS } from '../utils/constant'
-import { useDispatch, useSelector } from 'react-redux'
-import { addTrailerVideo } from '../utils/moviesSlice'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import useTrailerVideo from '../hooks/useTrailerVideo'
 
 const VideoBackground = ({movieId}) => {
 
   const trailerVideo = useSelector(store => store.movies?.trailerVideo)
 
-  // const [trailerId, setTrailerId] = useState(null);
-  const dispatch = useDispatch()
+  useTrailerVideo({movieId})
 
-  const getMovieVideos = async() =>{
-    const data = await fetch('https://api.themoviedb.org/3/movie/'+ movieId +'/videos?language=en-US', TMDB_API_OPTIONS)
-    const json = await data.json()
-    // console.log(json)
-
-    const filterData = json.results.filter(video=> video.type === "Trailer")
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer)
-
-    // setTrailerId(trailer.key)
-    dispatch(addTrailerVideo(trailer))
-  }
-
-  useEffect(()=>{
-    getMovieVideos()
-  }, [])
   return (
     <div>
       <iframe
