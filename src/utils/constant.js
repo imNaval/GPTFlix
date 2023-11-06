f939d2c004e46307d1577e2323bab1971b1ad2fb
imNaval <nehranvl2017@gmail.com>
2023-11-06 18:29:39 +0530

    env file and all

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,14 +3,14 @@ export const BG_IMG = 'https://assets.nflxext.com/ffe/siteui/vlv3/9db4a880-3034-
 export const GPTFLIX_LOGO = 'https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
 export const USER_LOGO = 'https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg'
 
-export const TMDB_API_KEYS = "cd904eec0e41c3d5f3d9681d72c1af47"
-export const TMDB_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDkwNGVlYzBlNDFjM2Q1ZjNkOTY4MWQ3MmMxYWY0NyIsInN1YiI6IjY1MWMxY2I1ZWE4NGM3MDBlYjlhZTNhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.e787-PTHCzOMomacwy-_p8h98LcRGZ6SpMjYj014A0g"
+export const TMDB_API_KEYS = process.env.REACT_APP_TMDB_API_KEYS
+export const TMDB_ACCESS_TOKEN = process.env.REACT_APP_TMDB_ACCESS_TOKEN
 export const TMDB_API_OPTIONS = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDkwNGVlYzBlNDFjM2Q1ZjNkOTY4MWQ3MmMxYWY0NyIsInN1YiI6IjY1MWMxY2I1ZWE4NGM3MDBlYjlhZTNhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.e787-PTHCzOMomacwy-_p8h98LcRGZ6SpMjYj014A0g'
-    }
+      Authorization: 'Bearer ' + TMDB_ACCESS_TOKEN
+       }
   };
 
   export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500"
@@ -21,4 +21,4 @@ export const TMDB_API_OPTIONS = {
     {identifier: 'es', name: "Spanish"}
   ]
 
-  export const OPENAI_KEY = "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"
+  export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY
